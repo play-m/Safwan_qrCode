2e5cb20c5f7368b17d2e3f7249407a6e44ce6c05
pl@y~M@k3r <iamplaymaker7@gmail.com>
2024-09-10 22:14:25 +0100

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-4444982234880637"></meta>
+      </head>
       <body className="bg-black">
         <Navbar />
         {children}
